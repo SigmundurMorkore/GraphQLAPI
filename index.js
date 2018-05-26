16b792aeda0d9c9c41f2281a36267fec6ee5baa9
mylbp2ps3 <sisomagmomunden@gmail.com>
2018-05-26 23:05:59 +0100

    Added paintings api route

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const hapi = require("hapi")
 const mongoose = require("mongoose")
+const Painting = require("./models/Painting")
 
 mongoose.connect(
   "mongodb://sigmundur:Testing1234@ds237620.mlab.com:37620/graphql"
@@ -15,13 +16,36 @@ const server = hapi.server({
 })
 
 const init = async () => {
-  server.route({
-    method: "GET",
-    path: "/",
-    handler: (request, reply) => {
-      return `<h1>My modern api</h1>`
+  server.route([
+    {
+      method: "GET",
+      path: "/",
+      handler: (request, reply) => {
+        return `<h1>My modern api</h1>`
+      }
+    },
+    {
+      method: "GET",
+      path: "/api/v1/paintings",
+      handler: (req, reply) => {
+        return Painting.find()
+      }
+    },
+    {
+      method: "POST",
+      path: "/api/v1/paintings",
+      handler: (req, reply) => {
+        const { name, url, techniques } = req.payload
+        const painting = new Painting({
+          name,
+          url,
+          techniques
+        })
+
+        return painting.save()
+      }
     }
-  })
+  ])
 
   await server.start()
   console.log(`Server running at: ${server.info.uri}`)
